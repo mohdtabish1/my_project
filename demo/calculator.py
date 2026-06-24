b5fa26dc2e831b829eb991c53cdd307c6a3ab641
mohd Tabish <mrtabish123@gmail.com>
2026-06-24 19:26:40 +0530

    Edits into calculator

--- demo/calculator.py
+++ demo/calculator.py
@@ -1,15 +1,6 @@
-def subtract(a,b,c,d):
-    return a-b-c-d
+def subtract(a, b, c, d):
+    return a - b - c - d
 
-def add(a,b,c,d):
 
-    if a is None:
-        a = 0
-    if b is None:
-        b = 0
-    if c is None:
-        c = 0
-    if d is None:
-        d = 0
-    return a+b+c+d
-
+def add(a, b, c, d):
+    return a + b + c + d
